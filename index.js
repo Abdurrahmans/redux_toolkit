531542150da5809cc6407b8aadf21465fdb5e999
abdur rahman <abdurrahmansmark1997@gmail.com>
2022-10-01 22:17:59 +0600

    complete counter app

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ const {createStore} =require("redux");
 // definig constants
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
 
 
 //State
 const InitialConuterState ={
     count: 0,
-
 };
 
 //action-object-type, payload
 const incrementCounter = () =>{
     return{
         type: INCREMENT,
-
     };
-
 };
 
 const decrementCounter = () =>{
     return{
         type: DECREMENT,
+    };
 
+};
+
+const resetCounterAction = () =>{
+    return{
+        type: RESET,
     };
 
 };
@@ -35,17 +39,20 @@ const counterReducer = (state=InitialConuterState, action) =>{
             return{
                 ... state,
                 count: state.count+1,
-
             };
         case DECREMENT:
             return{
                 ... state,
                 count: state.count-1,
-    
+            };
+
+        case RESET:
+            return{
+                ... state,
+                count: 0,
             };
         default:
             state;
-
 
     };
 
@@ -63,3 +70,11 @@ store.subscribe(()=>{
 //dispatch action
 
 store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(decrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(resetCounterAction())
+store.dispatch(incrementCounter())
+
+
